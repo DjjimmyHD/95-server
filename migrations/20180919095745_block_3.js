9fd083fb655a8ee7a92827ad5cf3ffbae24cc8e5
Patrick Biffle <pbiffle@gmail.com>
2018-09-19 10:03:43 -0600

    added block 3 and block 4 seed files

--- migrations/20180919095745_block_3.js
+++ migrations/20180919095745_block_3.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('block_3', (block) => {
+      block.increments('id')
+      block.string('name')
+      block.string('curriculum')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('block_3')
 };
--- migrations/20180919095750_block_4.js
+++ migrations/20180919095750_block_4.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('block_4', (block) => {
+      block.increments('id')
+      block.string('name')
+      block.string('curriculum')
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('block_4')
 };
